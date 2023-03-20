54dd509cd6b753116125da867bb2821b1a6667de
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-20 01:37:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# inventory-sequence
+This script generates the [inventory sequence](https://www.youtube.com/watch?v=rBU9E-ZOZAI) (see also [here](https://oeis.org/search?q=inventory&language=english&go=Search)). 
---
+++ inventory_sequence.py
@@ -0,0 +1,31 @@
+# take_inventory.py
+# Andrew Lounsbury
+# 20/3/23
+# Purpose: generate the inventory sequence; https://www.youtube.com/watch?v=rBU9E-ZOZAI
+sequence = [0]
+highestNum = 0
+
+def count_num(num, sequence):
+    total = 0
+    for s in sequence:
+        if s == num:
+            total += 1
+    return total
+
+def take_inventory(sequence, n):
+    num = 0
+    # while the sequence has less than n numbers
+    while len(sequence) < n:
+        print("len:", len(sequence))
+        if sequence[len(sequence) - 1] == 0:
+            print("last:", sequence[len(sequence) - 1])
+            i = 0
+            while i < 100 and len(sequence) < n:
+                val = count_num(i, sequence)
+                sequence.append(val)
+                i += 1
+                if val == 0:
+                    i = 0
+
+take_inventory(sequence, 100)
+print(sequence)
